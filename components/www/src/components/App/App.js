18ae88e1e80dec16363dd2f830d9264cec9eb314
Mihail Novikov <thepocp@gmail.com>
2018-09-09 21:01:57 +0300

    Add deleting confirm

--- components/www/src/components/App/App.js
+++ components/www/src/components/App/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import List from '../List';
 
 class App extends Component {
+    changeRow = (name, value) => {
+        console.log(name, value);
+    };
+
+    deleteRow = key => {
+        console.log('delete', key);
+    };
     render() {
-        return <List />;
+        return <List deleteRow={this.deleteRow} changeRow={this.changeRow} />;
     }
 }
 
--- components/www/src/components/List/List.js
+++ components/www/src/components/List/List.js
@@ -1,5 +1,7 @@
-import React, { Component, Fragment } from 'react';
-import { Column, Table, Cell } from '@blueprintjs/table';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Button, Dialog, Classes } from '@blueprintjs/core';
+import { Column, Table, EditableCell, Cell } from '@blueprintjs/table';
 import Filter from '../Filter';
 import Pagination from '../Pagination';
 import rawData from './data';
@@ -23,12 +25,23 @@ const getDataSlice = (data, currentPage, pageSize) => {
     return data.slice(start, end);
 };
 
+const ListWrapper = styled.div`
+    padding: 20px;
+`;
+
+const PaginationWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
 class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: [],
             total: 0,
+            deletingRow: null,
+            showDialog: false,
             pageSize: 25,
             currentPage: 0,
             computedData: [],
@@ -54,28 +67,91 @@ class List extends Component {
 
     pageChangeHandler = currentPage => this.setState({ currentPage }, this.computeData);
 
-    cellRenderer = (index, key) => <Cell>{this.state.computedData[index][key]}</Cell>;
+    cellChangeHandler = (value, key, row) => {
+        const { computedData } = this.state;
+        const newValue = { ...computedData.find(({ name }) => name === row), [key]: value };
+        this.props.changeRow(row, newValue);
+    };
+
+    cellRenderer = (index, key) => (
+        <EditableCell
+            wrapText
+            rowIndex={key}
+            interactive
+            onConfirm={this.cellChangeHandler}
+            value={this.state.computedData[index][key]}
+            columnIndex={this.state.computedData[index].name}
+        />
+    );
+
+    controlsColumnRender = index => {
+        const { deleteRow } = this.props;
+        return (
+            <Cell style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button
+                    loading={false}
+                    small
+                    icon="trash"
+                    intent="danger"
+                    onClick={() => this.openDialog(this.state.computedData[index].name)}
+                />
+            </Cell>
+        );
+    };
+
+    openDialog = deletingRow => this.setState({ deletingRow, showDialog: true });
+
+    hideDialog = () => this.setState({ deletingRow: null, showDialog: false });
+
+    deleteRow = () => {
+        this.props.deleteRow(this.state.deletingRow);
+        this.hideDialog();
+    };
 
     render() {
-        const { computedData, filterValue, currentPage, total, pageSize } = this.state;
-        const dataLength = computedData.length;
+        const { computedData, filterValue, currentPage, total, pageSize, showDialog } = this.state;
+        const amount = computedData.length > pageSize ? pageSize : computedData.length;
+        const columnWidth = this.wrapper ? (this.wrapper.clientWidth - 90) / 3 : 0;
 
-        const amount = dataLength > pageSize ? pageSize : dataLength;
         return (
-            <Fragment>
+            <ListWrapper
+                innerRef={wrapper => {
+                    this.wrapper = wrapper;
+                }}
+            >
+                <Dialog title="Confirm deletion" isOpen={showDialog} onClose={this.hideDialog}>
+                    <div className={Classes.DIALOG_BODY}>
+                        Are you sure you want to delete this record?
+                    </div>
+                    <div className={Classes.DIALOG_FOOTER}>
+                        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                            <Button onClick={this.hideDialog}>Cancel</Button>
+                            <Button intent="danger" onClick={this.deleteRow}>
+                                Delete
+                            </Button>
+                        </div>
+                    </div>
+                </Dialog>
                 <Filter value={filterValue} handler={this.filterChangeHandler} />
-                <Table numRows={amount}>
+                <Table
+                    enableRowHeader={false}
+                    numRows={amount}
+                    columnWidths={[columnWidth, columnWidth, columnWidth, 50]}
+                >
                     <Column name="Name" cellRenderer={index => this.cellRenderer(index, 'name')} />
                     <Column name="Ru" cellRenderer={index => this.cellRenderer(index, 'ru')} />
                     <Column name="En" cellRenderer={index => this.cellRenderer(index, 'en')} />
+                    <Column name="" cellRenderer={this.controlsColumnRender} />
                 </Table>
-                <Pagination
-                    total={total}
-                    current={currentPage}
-                    handler={this.pageChangeHandler}
-                    pageSize={pageSize}
-                />
-            </Fragment>
+                <PaginationWrapper>
+                    <Pagination
+                        total={total}
+                        current={currentPage}
+                        handler={this.pageChangeHandler}
+                        pageSize={pageSize}
+                    />
+                </PaginationWrapper>
+            </ListWrapper>
         );
     }
 }
