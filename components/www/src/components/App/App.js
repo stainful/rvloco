fdbc4463ebcfdaeca862273c3f24c0ea80df6006
Mihail Novikov <thepocp@gmail.com>
2018-09-10 14:53:53 +0300

    Add mocks for api methods

--- components/www/src/components/App/App.js
+++ components/www/src/components/App/App.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import List from '../List';
+import NewRecord from '../NewRecord';
+
+import rawData from '../../data';
+
+const Wrapper = styled.div`
+    margin: 20px;
+`;
 
 class App extends Component {
-    changeRow = (name, value) => {
-        console.log(name, value);
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            updateHandler: () => null,
+        };
+    }
+    componentDidMount() {
+        this.setState({ loading: true });
+        setTimeout(() => {
+            const data = Object.entries(rawData).map(([name, { ru, en }]) => ({ name, en, ru }));
+            this.setState({ data, loading: false }, this.state.updateHandler);
+        }, 1500);
+    }
+
+    setUpdateHandler = updateHandler => this.setState({ updateHandler });
+
+    changeRow = (name, value, { onSuccess, onError }) => {
+        setTimeout(() => {
+            console.log(name, value);
+            onSuccess();
+        }, 1000);
     };
 
-    deleteRow = key => {
-        console.log('delete', key);
+    deleteRow = (key, { onSuccess, onError }) => {
+        setTimeout(() => {
+            console.log('delete', key);
+            onSuccess();
+        }, 1000);
     };
+
+    create = (key, body, { onSuccess, onError }) => {
+        setTimeout(() => {
+            console.log(key, body);
+            onSuccess();
+        }, 1000);
+    };
+
     render() {
-        return <List deleteRow={this.deleteRow} changeRow={this.changeRow} />;
+        const { loading, data } = this.state;
+        return (
+            <Wrapper>
+                <NewRecord create={this.create} />
+                <List
+                    data={data}
+                    loading={loading}
+                    deleteRow={this.deleteRow}
+                    changeRow={this.changeRow}
+                    setUpdateHandler={this.setUpdateHandler}
+                />
+            </Wrapper>
+        );
     }
 }
 
