60617d0189ed95a6c16a1de86c14969804d394d4
Mihail Novikov <thepocp@gmail.com>
2018-10-02 18:55:44 +0300

    Add clear button to filter

--- components/www/src/components/Filter/Filter.js
+++ components/www/src/components/Filter/Filter.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { InputGroup } from '@blueprintjs/core';
+import { InputGroup, Tooltip, Button } from '@blueprintjs/core';
 import styled from 'styled-components';
 
 const StyledInput = styled(InputGroup)`
     margin-bottom: 10px;
 `;
 
-const Filter = ({ value, handler }) => (
-    <StyledInput
-        leftIcon="filter"
-        placeholder="Filter"
-        value={value}
-        onChange={({ target }) => handler(target.value)}
-    />
-);
+const Filter = ({ value, handler }) => {
+    const ClearButton = (
+        <Tooltip content="Clear" disabled={false}>
+            <Button
+                disabled={false}
+                icon="cross"
+                minimal={true}
+                onClick={() => handler('')}
+            />
+        </Tooltip>
+    );
+
+    return (
+        <StyledInput
+            leftIcon="filter"
+            rightElement={ClearButton}
+            placeholder="Filter"
+            value={value}
+            onChange={({ target }) => handler(target.value)}
+        />
+    );
+}
 
 Filter.defaultProps = {
     value: '',
